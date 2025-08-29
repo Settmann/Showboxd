f6a9e57611ca59e29d7bab8b3ab5015e6b56a6d2
SettMann <s.mannarino137@gmail.com>
2025-08-28 20:32:01 -0400

    just some work on the laptop, added homepage and showcard, needs testing and styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
+import Homepage from './components/Homepage.jsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Navbar></Navbar>
+        <Homepage></Homepage>
       </header>
       
     </div>
---
+++ client/src/components/ShowCard.jsx
@@ -0,0 +1,13 @@
+import "./ShowCard.css";
+import React, { useEffect, useState } from 'react';
+
+export default function ShowCard(props) {
+  //check the props to make sure the names are good
+  return (
+    <div class="nav flex-center space-between">
+      <img src={props.poster}></img> 
+      <p>{props.rating}</p>
+      <h3>{props.title}</h3>
+    </div>
+  );
+}
